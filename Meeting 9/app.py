7fb42b8d4c203355895770504b71d5d06b172eaa
Alex Moore <amoore2829@gmail.com>
2022-11-03 21:02:14 -0400

    #min triangle sum problem

---
+++ Meeting 9/app.py
@@ -0,0 +1,15 @@
+#min triangle sum problem
+
+n = int(input())
+
+triangle = []
+
+for _ in range(n):
+    row = list(map(int, input().split())
+
+    triangle.append(row)
+
+cache = []
+
+for i in range(n):
+    cache.append([])
